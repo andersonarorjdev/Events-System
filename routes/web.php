5938ea7f90ac6c2b58d7b0218f112e2396a79e6f
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2021-02-12 12:00:34 -0300

    Maked first controller to add posts on database

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\{
+    PostController
+};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PhotoController');
+Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/', function () {
+    return view('index');
+});
