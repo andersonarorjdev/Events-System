5c50911f3678fa1f1d12209e86ff83454670420c
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2021-03-08 19:06:22 -0300

    Added a new stylesheet to style the about page

--- resources/views/events/about.blade.php
+++ resources/views/events/about.blade.php
@@ -1,6 +1,8 @@
-@extends('layouts.main');
-
-@section('content');
-    <h1>Hello World</h1>
-
+<head>
+    <link rel="stylesheet" href="/css/about/about.css">
+</head>
+@extends('layouts.main')
+    <title>@section('title', 'Name')</title>
+@section('content')
+         <h1>Here will be some information about this page or website</h1>
 @endsection
